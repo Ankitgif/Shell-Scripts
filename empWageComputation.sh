371e6deb90738562be55e3e3284509b8ff75be8b
Ankitgif <ankitsinha2303@gmail.com>
2020-03-20 18:43:02 +0530

    Added functions for solving using case statement and declared WAGEPERHOUR as constant

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,39 @@
 echo "Welcome to Employee Wage Computation Program"
-checkAttendance=$((RANDOM%2))
-if (( $checkAttendance == 1 ))
-then
-	echo "Employee is Present."
-else
-	echo "Employee is Absent."
-fi
-wagePerhour=20
-fullDayhour=8
-dailyEmpWage=$((wagePerhour*fullDayhour))
-echo "Daily Employee Wage is $dailyEmpWage"
-partTimehour=4
-partTimeEmpWage=$((wagePerhour*partTimehour))
-echo "Part time employee wage is $partTimeEmpWage"
+attendanceCheck()
+{
+	checkAttendance=$((RANDOM%2))
+	if (( $checkAttendance == 1 ))
+	then
+		echo "Employee is Present."
+	else
+		echo "Employee is Absent."
+	fi
+}
+WAGEPERHOUR=20
+dailyEmployeewage()
+{
+	fullDayhour=8
+	dailyEmpWage=$((WAGEPERHOUR*fullDayhour))
+	echo "Daily Employee Wage is $dailyEmpWage"
+}
+partTimeemployeewage()
+{
+	partTimehour=4
+	partTimeEmpWage=$((WAGEPERHOUR*partTimehour))
+	echo "Part time employee wage is $partTimeEmpWage"
+}
+echo "Enter choice to do operation 1.Check Attendance 2.Calculate daily employee wage 3.Calculate Part time employee wage"
+read num
+choice=$num
+case $choice in
+1) attendanceCheck
+   ;;
+2) dailyEmployeewage
+   ;;
+3) partTimeemployeewage
+   ;;
+*)
+  echo "Please provide valid choice"
+esac
+
+
